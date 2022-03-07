a9fc317a792f7e77455dc08ea9234cb1032fe369
Nox <benaziz.ines@gmail.com>
2022-03-07 18:00:09 +0100

    refractorisation of wire drawing and timer fully functionnal

--- Script/Visual/Bomb.js
+++ Script/Visual/Bomb.js
@@ -2,59 +2,45 @@ const canvas = document.getElementById('canvas')
 const context = canvas.getContext('2d')
 
 // . . . . . . . . . . . . . . . . . . . . . DRAWING THE WIRES . . . . . . . . . . . . . . . . . 
-//Drawing wire top left
-context.beginPath()
-context.moveTo(100, 150)
-context.arc(240, 150, 130, Math.PI, Math.PI*2)
-context.lineWidth = 18
-context.strokeStyle = 'orange'
-context.stroke()
-context.closePath()
 
-//Drawing wire top right
-context.beginPath()
-context.moveTo(295, 150)
-context.arc(400, 150, 120, Math.PI, Math.PI*2)
-context.lineWidth = 25
-context.strokeStyle = 'black'
-context.stroke()
-context.closePath()
+class Wires {
+    constructor(x1, y1, x2, y2, radius, startAngle, endAngle, strokeWidth, color) {
+        this.x1 = x1
+        this.y1 = y1
+        this.x2 = x2
+        this.y2 = y2
+        this.radius = radius
+        this.startAngle = startAngle
+        this.endAngle = endAngle
+        this.strokeWidth = strokeWidth
+        this.color = color
+    }
 
-//Drawing wire right top
-context.beginPath()
-context.moveTo(600, 200)
-context.arc(600, 300, 100, (Math.PI*3)/2, Math.PI/2)
-context.lineWidth = 20
-context.strokeStyle = 'red'
-context.stroke()
-context.closePath()
+    id = 0
 
-//Drawing wire right bottom
-context.beginPath()
-context.moveTo(600, 300)
-context.arc(600, 400, 100, (Math.PI*3)/2, Math.PI/2)
-context.lineWidth = 10
-context.strokeStyle = 'green'
-context.stroke()
-context.closePath()
+    draw() {
+        context.beginPath()
+        context.moveTo(this.x1, this.y1)
+        context.arc(this.x2, this.y2, this.radius, this.startAngle, this.endAngle)
+        context.lineWidth = this.strokeWidth
+        context.strokeStyle = this.color
+        context.stroke()
+        context.closePath()
+    }
+}
 
-//Drawing wire bottom left
-context.beginPath()
-context.moveTo(300, 500)
-context.arc(200, 500, 95, Math.PI*2, Math.PI)
-context.lineWidth = 20
-context.strokeStyle = 'purple'
-context.stroke()
-context.closePath()
+const wires = [
+    new Wires(100, 150, 240, 150, 130, Math.PI, Math.PI*2, 18, 'orange'),
+    new Wires(295, 150, 400, 150, 120, Math.PI, Math.PI*2, 25, 'black'),
+    new Wires(600, 200, 600, 300, 100, (Math.PI*3)/2, Math.PI/2, 20, 'red'),
+    new Wires(600, 300, 600, 400, 100, (Math.PI*3)/2, Math.PI/2, 10, 'green'),
+    new Wires(300, 500, 200, 500, 95, Math.PI*2, Math.PI, 20, 'purple'),
+    new Wires(500, 500, 400, 500, 120, Math.PI*2, Math.PI, 15, 'pink')
+]
 
-//Drawing wire bottom right
-context.beginPath()
-context.moveTo(500, 500)
-context.arc(400, 500, 120, Math.PI*2, Math.PI)
-context.lineWidth = 15
-context.strokeStyle = 'pink'
-context.stroke()
-context.closePath()
+for (i=0; i < wires.length; i++) {
+    wires[i].draw()
+}
 
 // . . . . . . . . . . . . . . . . . . . . . DRAWING THE BOMB . . . . . . . . . . . . . . . . . 
 
@@ -95,7 +81,7 @@ context.closePath()
 //Timer text
 
 function startTimer() {
-    let sec = 10
+    let sec = 30
     setInterval(function () {
         context.clearRect(180, 290, 330, 80)
         context.fillStyle = 'red'
@@ -110,9 +96,7 @@ function startTimer() {
     }, 1000)
 }
 
-
-window.onload = function () {
+document.querySelector('.start-btn').addEventListener('click', function() {
     startTimer()
-}
-
+ })
 
