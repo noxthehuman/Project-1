938152a0f37fd4d1a174f1b19748f0d26c30797e
Nox <benaziz.ines@gmail.com>
2022-03-07 10:36:53 +0100

    first two wires drawn

--- Logic.js
+++ Logic.js
@@ -1,12 +1,24 @@
-const canvas = document.getElementsById('canvas');
+const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 
-//Drawing the wires
+//Drawing wire top left
 context.beginPath()
 context.moveTo(100, 300)
-context.arc(100, 300, 50, 0, Math.PI*2)
-context.lineWidth = 30
-context.strokeStyle = 'purple'
+context.arc(250, 300, 150, Math.PI, Math.PI*2)
+context.lineWidth = 25
+context.strokeStyle = 'orange'
 context.stroke()
 context.closePath()
 
+//Drawing wire top right
+context.beginPath()
+context.moveTo(400, 300)
+context.arc(450, 300, 120, Math.PI*2, Math.PI, true)
+context.lineWidth = 25
+context.strokeStyle = 'blue'
+context.stroke()
+context.closePath()
+
+//Drawing wire right
+
+console.log(canvas)
