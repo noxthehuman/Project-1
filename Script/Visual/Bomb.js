081859c7e93a5c0a10f488e5ee9f8f275a429688
Nox <benaziz.ines@gmail.com>
2022-03-08 14:34:40 +0100

    basic logic implemented

--- Script/Visual/Bomb.js
+++ Script/Visual/Bomb.js
@@ -1,10 +1,8 @@
 const canvas = document.getElementById('canvas')
 const context = canvas.getContext('2d')
 
-// . . . . . . . . . . . . . . . . . . . . . DRAWING THE WIRES . . . . . . . . . . . . . . . . . 
-
 class Wires {
-    constructor(x1, y1, x2, y2, radius, startAngle, endAngle, strokeWidth, color) {
+    constructor(x1, y1, x2, y2, radius, startAngle, endAngle, strokeWidth, color, id) {
         this.x1 = x1
         this.y1 = y1
         this.x2 = x2
@@ -14,37 +12,46 @@ class Wires {
         this.endAngle = endAngle
         this.strokeWidth = strokeWidth
         this.color = color
-    }
+        this.id = id  
+    }   
 
-    id = 0
-
-    draw() {
+    draw(newColor) {
         context.beginPath()
         context.moveTo(this.x1, this.y1)
         context.arc(this.x2, this.y2, this.radius, this.startAngle, this.endAngle)
         context.lineWidth = this.strokeWidth
-        context.strokeStyle = this.color
+        context.strokeStyle = this.color || newColor
         context.stroke()
         context.closePath()
     }
 }
 
 const wires = [
-    new Wires(100, 150, 240, 150, 130, Math.PI, Math.PI*2, 18, 'orange'),
-    new Wires(295, 150, 400, 150, 120, Math.PI, Math.PI*2, 25, 'black'),
-    new Wires(600, 200, 600, 300, 100, (Math.PI*3)/2, Math.PI/2, 20, 'red'),
-    new Wires(600, 300, 600, 400, 100, (Math.PI*3)/2, Math.PI/2, 10, 'green'),
-    new Wires(300, 500, 200, 500, 95, Math.PI*2, Math.PI, 20, 'purple'),
-    new Wires(500, 500, 400, 500, 120, Math.PI*2, Math.PI, 15, 'pink')
+    new Wires(100, 150, 240, 150, 130, Math.PI, Math.PI*2, 18, 'orange', 0),
+    new Wires(295, 150, 400, 150, 120, Math.PI, Math.PI*2, 25, 'black', 0),
+    new Wires(600, 200, 600, 300, 100, (Math.PI*3)/2, Math.PI/2, 20, 'red', 0),
+    new Wires(600, 300, 600, 400, 100, (Math.PI*3)/2, Math.PI/2, 10, 'green', 0),
+    new Wires(300, 500, 200, 500, 95, Math.PI*2, Math.PI, 20, 'purple', 0),
+    new Wires(500, 500, 400, 500, 120, Math.PI*2, Math.PI, 15, 'pink', 0)
 ]
-
-for (i=0; i < wires.length; i++) {
-    wires[i].draw()
+    
+for (i=0; i < 3; i++) {
+    let randomId = Math.floor(Math.random() * 3)
+    let randomIndex = Math.floor(Math.random() * wires.length)   
+    
+    if(wires[randomIndex].id !== randomId) {
+        wires[randomIndex].id = randomId
+    }
 }
 
-// . . . . . . . . . . . . . . . . . . . . . DRAWING THE BOMB . . . . . . . . . . . . . . . . . 
+function drawWires(i, newColor) {
+    for (i=0; i < wires.length; i++) {
+        wires[i].draw(newColor)
+    }
+}
 
-context.fillStyle = 'grey'
+function drawBomb() {
+    context.fillStyle = 'grey'
 context.fillRect(70, 140, 550, 380)
 
 context.fillStyle = 'black'
@@ -77,11 +84,37 @@ context.arc(600, 500, 10, 0, Math.PI*2)
 context.fillStyle = 'black'
 context.fill()
 context.closePath()
+}
+
+function getMousePosition( canvas, evt ) {
+    const rect = canvas.getBoundingClientRect();
+    return {
+        x: Math.floor( ( evt.clientX - rect.left ) / ( rect.right - rect.left ) * canvas.width ),
+        y: Math.floor( ( evt.clientY - rect.top ) / ( rect.bottom - rect.top ) * canvas.height )
+    };
+}
+
+function WinCondition() {
+    clearInterval(sec)
+    context.clearRect(0, 0, canvas.width, canvas.height)
+    context.fillStyle = 'green'
+    context.fillRect(0, 0, canvas.width, canvas.height)
+}
+
+function loseCondition() {
+    clearInterval(sec)
+    context.clearRect(0, 0, canvas.width, canvas.height)
+    context.fillStyle = 'red'
+    context.fillRect(0, 0, canvas.width, canvas.height)
+}
+
+function penaltyTime() {
+    sec -= sec/4
+}
 
 //Timer text
-
+let sec = 30
 function startTimer() {
-    let sec = 30
     setInterval(function () {
         context.clearRect(180, 290, 330, 80)
         context.fillStyle = 'red'
@@ -98,5 +131,36 @@ function startTimer() {
 
 document.querySelector('.start-btn').addEventListener('click', function() {
     startTimer()
- })
+    drawWires()
+    drawBomb()
+    canvas.addEventListener('click', function( evt) {
+        let mousePos = getMousePosition( canvas, evt );
+        
+        for (i=0; i < wires.length; i++) {
+            if(mousePos.x < wires[i].x2 && mousePos.y < wires[i].y2) {
+                if(wires[i].id === 1) {
+                    WinCondition()
+                }
+                if(wires[i].id === 2) {
+                    loseCondition()
+                }
+                if(wires[i].id === 3) {
+                    penaltyTime()
+                }
+            }
+        }
+    })
+})
+
+
+canvas.addEventListener( 'mouseenter', function( evt ) {
+    let mousePos = getMousePosition( canvas, evt );
+    if(mousePos.x < 250 && mousePos.y < 130) {
+        
+    }
+
+} );
+
+
+
 
